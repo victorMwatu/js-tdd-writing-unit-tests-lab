a2e0abefdcec336892b0c97e2864a8cb2f48a1da
victorMwatu <victormwatu@gmail.com>
2025-07-26 14:09:42 +0300

    complete js tdd wiriting unit tests lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe( "isPalindrome", () => {
+    it("returns true if word is palindrome and false if it isn't", () => {
+        const notPalindrome = isPalindrome("car");
+        const palindrome = isPalindrome("racecar");
+        const blank = isPalindrome("");
+        expect(palindrome).toBe(true);
+        expect(notPalindrome).toBe(false);
+        expect(blank).toBe(false);
+    });
+    it("throws typeError if word is not a string", () => {
+        expect(()=> isPalindrome(234)).toThrow(TypeError);
+    });
+    it("retruns false if word is empty string", ()=> {
+        expect(isPalindrome("")).toBe(false);
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
-// Your code here
+export function isPalindrome(word) {
+    if(!word) return false;
+    let reverse = "";
+    if(typeof(word) !== "string") {
+        throw new TypeError("Input not");
+    }
+    for(const l of word){
+       reverse = l + reverse;
+    }
+    return reverse === word;
+}
+
+console.log(isPalindrome(""));
